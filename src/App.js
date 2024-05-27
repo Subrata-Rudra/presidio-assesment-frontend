9e0bc39c72ca3197419b7a2904df33744f67af44
Subrata-Rudra <rudrasubrata27@gmail.com>
2024-05-27 16:04:57 +0530

    ChakraProvider is added to provide styling to the UI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import AuthenticationPage from "./pages/authenticationPage";
+import { useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  useEffect(async () => {
+    try {
+      const url = process.env.REACT_APP_SERVER_URL + "/test";
+      const response = await axios.get(url);
+      if (response.status === 200) {
+        alert("Server is ready🎉");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  });
   return (
     <div className="App">
       <Routes>
